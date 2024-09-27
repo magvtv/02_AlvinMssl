1291aa92883602831f3b64b8651e0092fdc98289
magvtv <magutujr@gmail.com>
2024-09-27 10:15:34 +0300

    working on the breakpoints for larger devices

--- Revamp/assets/js/main.js
+++ Revamp/assets/js/main.js
@@ -38,10 +38,10 @@ bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
 const swiperServices = new Swiper('.services__swiper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 24,
-    slidesPerView: 'auto',
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 24,
+	slidesPerView: 'auto',
 
 	// Navigation arrows
 	navigation: {
@@ -51,7 +51,33 @@ const swiperServices = new Swiper('.services__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up');
+	// When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+scrollUp();
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+	const scrollDown = window.scrollY;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id'),
+			sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link');
+		} else {
+			sectionsClass.classList.remove('active-link');
+		}
+	});
+};
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
